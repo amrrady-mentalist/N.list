7f705bc44198e8e75e005a7884f2165463fff0c8
amrrady-mentalist <amrrady4020@gmail.com>
2026-08-14 16:21:52 +0300

    Update NoteEditScreen.kt

--- app/src/main/java/com/notesapp/offline/ui/NoteEditScreen.kt
+++ app/src/main/java/com/notesapp/offline/ui/NoteEditScreen.kt
@@ -3,13 +3,13 @@ package com.notesapp.offline.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.imePadding
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
