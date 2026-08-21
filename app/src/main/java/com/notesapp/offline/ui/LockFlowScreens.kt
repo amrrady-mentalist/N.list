d173e1f6522aed180a4a8cccd8b0b80df6eeeb68
amrrady-mentalist <amrrady4020@gmail.com>
2026-08-21 06:43:51 +0300

    Update LockFlowScreens.kt

--- app/src/main/java/com/notesapp/offline/ui/LockFlowScreens.kt
+++ app/src/main/java/com/notesapp/offline/ui/LockFlowScreens.kt
@@ -70,7 +70,7 @@ import java.util.Locale
 import kotlin.math.abs
 
 @Composable
-fun LockFlowHost(viewModel: LockFlowViewModel, isDarkTheme: Boolean) {
+fun LockFlowHost(viewModel: LockFlowViewModel) {
     val screen by viewModel.screen.collectAsState()
     val backgroundPath by viewModel.lockBackgroundPath.collectAsState()
 
@@ -89,7 +89,7 @@ fun LockFlowHost(viewModel: LockFlowViewModel, isDarkTheme: Boolean) {
             }
         }
         LockScreenState.PIN -> PinScreen(viewModel, backgroundPath)
-        LockScreenState.HOME_SCREEN -> HomeScreenFlowScreen(viewModel, isDarkTheme)
+        LockScreenState.HOME_SCREEN -> HomeScreenFlowScreen(viewModel)
     }
 }
 
